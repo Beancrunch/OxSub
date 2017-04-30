540b90e34c9d7286098b781b54fda4aa6f01a20c
Anurag Kapur <anuragkapur@gmail.com>
2017-05-01 00:51:43 +0100

    Add static upcoming outings section

--- src/UpcomingOutings.js
+++ src/UpcomingOutings.js
@@ -1,5 +1,27 @@
 import React, { Component } from 'react';
 
+class RequestSummary extends Component {
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <div className="col-md-8">
+            <div className="left-align">
+                Christ Church M2 looking for a Cox <br />
+                For an outing at 2pm on Saturday 29 April 2017.
+            </div>
+          </div>
+          <div className="col-md-4">
+            <button type="button" className="btn btn-sm btn-default">Respond</button>
+          </div>
+        </div>
+        <hr />
+      </div>
+
+    );
+  }
+}
+
 class UpcomingOutings extends Component {
   render() {
     return (
@@ -7,7 +29,18 @@ class UpcomingOutings extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-8">
-              <h3>Upcoming Outings</h3>
+              <div className="panel panel-default">
+                <div className="panel-heading">
+                  <h3 className="panel-title">Upcoming Outings</h3>
+                </div>
+                <div className="panel-body">
+                  <RequestSummary />
+                  <RequestSummary />
+                  <RequestSummary />
+                  <RequestSummary />
+                  <RequestSummary />
+                </div>
+              </div>
             </div>
             <div className="col-md-4">
               <button type="button" className="btn btn-lg btn-primary">CREATE A NEW REQUEST</button>
