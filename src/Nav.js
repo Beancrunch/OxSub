16b3599b12f094e903cfff59866f05cba2f83042
Anurag Kapur <anuragkapur@gmail.com>
2017-04-30 21:13:33 +0100

    Refactor App to Nav

---
+++ src/Nav.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Nav extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-inverse navbar-fixed-top">
+        <div className="container">
+         <div className="navbar-header">
+           <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+             <span className="sr-only">Toggle navigation</span>
+             <span className="icon-bar"></span>
+             <span className="icon-bar"></span>
+             <span className="icon-bar"></span>
+           </button>
+           <a className="navbar-brand" href="#">OxSub</a>
+         </div>
+         <div id="navbar" className="collapse navbar-collapse">
+           <ul className="nav navbar-nav">
+             <li><a href="#">Need a Sub?</a></li>
+             <li><a href="#">Looking to Sub?</a></li>
+             <li><a href="#">Login</a></li>
+             <li><a href="#">Signup</a></li>
+           </ul>
+         </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Nav;
