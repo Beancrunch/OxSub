c46088e5e7d770460a1ab2817d2d5eacfed8d5b2
Anurag Kapur <anuragkapur@gmail.com>
2017-05-04 14:25:11 +0100

    Refactor for readability

--- src/Nav.js
+++ src/Nav.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { login, logout, isLoggedIn, getIdToken, getLock } from './AuthService';
 
+const PROFILE_TOKEN_KEY = 'profile_token'
+
 class Nav extends Component {
 
   constructor(props) {
@@ -9,21 +11,31 @@ class Nav extends Component {
   }
 
   componentDidMount() {
-    const PROFILE_TOKEN_KEY = 'profile_token';
-    if (isLoggedIn()) {
-      let lock = getLock()
-      let idToken = getIdToken()
-      lock.getProfile(idToken, (error, profile) => {
-        if (error) {
-          console.log('Error loading the Profile', error)
-        } else {
-          let profileString = JSON.stringify(profile)
-          console.log('got profile from server :: ' + profileString)
-          localStorage.setItem(PROFILE_TOKEN_KEY, profileString)
-          this.setState({profile: profile})
-        }
-      })
+    let profile = localStorage.getItem(PROFILE_TOKEN_KEY)
+    if (profile) {
+      console.log(profile)
+      this.setState({profile: JSON.parse(profile)})
+    } else if (isLoggedIn()){
+      console.log('profile not found in local storage but user is logged in')
+      this.getRemoteProfileAndSetToLocalStorageAndSetState()
+    } else {
+      console.log('profile not found in local storage and use is NOT logged in')
     }
+  }
+
+  getRemoteProfileAndSetToLocalStorageAndSetState() {
+    let lock = getLock()
+    let idToken = getIdToken()
+    lock.getProfile(idToken, (error, profile) => {
+      if (error) {
+        console.log('Error loading the Profile', error)
+      } else {
+        let profileString = JSON.stringify(profile)
+        console.log('got profile from server :: ' + profileString)
+        localStorage.setItem(PROFILE_TOKEN_KEY, profileString)
+        this.setState({profile: profile})
+      }
+    })
   }
 
   render() {
