6c258e33bc14809c54e1b05275b8826f689ef69b
Anurag Kapur <anuragkapur@gmail.com>
2017-05-05 17:58:46 +0100

    Force https on non localhost connections

--- src/components/routing/MainRouter.js
+++ src/components/routing/MainRouter.js
@@ -3,19 +3,22 @@ import {
   BrowserRouter as Router,
   Route
 } from 'react-router-dom';
+import HttpsRedirect from 'react-https-redirect';
 import Home from '../../pages/Home';
 import Signin from '../../pages/Signin';
 import Callback from '../auth0/Callback';
 
 const MainRouter = () => (
-  <Router>
-    <div>
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/home" component={Home}/>
-      <Route path="/login" component={Signin}/>
-      <Route path="/callback" component={Callback}/>
-    </div>
-  </Router>
+  <HttpsRedirect>
+    <Router>
+      <div>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/home" component={Home}/>
+        <Route path="/login" component={Signin}/>
+        <Route path="/callback" component={Callback}/>
+      </div>
+    </Router>
+  </HttpsRedirect>
 );
 
 export default MainRouter;
